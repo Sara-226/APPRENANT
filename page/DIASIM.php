c36b37a81a73b213b46a429b6b9857f7430dab66
Sara-226 <yidjabazie@gmail.com>
2021-01-22 14:24:06 +0100

    origin

---
+++ page/DIASIM.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <title>DIASIM</title>
+</head>
+<body>
+    <form action="traitement.php" method="post">
+        <input type="text" name="NOM" placeholder="NOM">
+        <input type="text" name="PRENOM" placeholder="PRENOM">
+        <input type="text" name="NAISSANCE" placeholder="NAISSANCE">
+        <input type="text" name="Email" placeholder="Email">
+        <input type="text" name="FORMATION" placeholder="FORMATION">
+        <input type="text" name="VILLE" placeholder="VILLE">
+        <input type="submit" value="envoyer">
+
+    </form>
+</body>
+</html>
+<style>
+    body{
+        text-align: center;
+    }
+</style>
---
+++ page/Untitled-1.php
@@ -0,0 +1,9 @@
+<?php
+$donnes; 
+$fin=0;
+while($fin< 200){
+    echo '<td><a>'.$donnes['nom'].'</a></td>';
+}
+?>
+
+<td><a href=""> </a> </td>
